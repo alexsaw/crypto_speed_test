591c7f502b71aa7a8cc066493a3ed161670e3de5
Alex <alex.sawsienowicz@gmail.com>
2020-07-04 16:33:27 -0700

    second commit

--- iot_client.py
+++ iot_client.py
@@ -1,8 +1,18 @@
-import cryptography
+import os
 import platform
 from uuid import getnode as get_mac
 import json
 import time
+
+import cryptography
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
 
 #
 # This program simulates an IOT that collects data from a connected sensor and sends the data in an encrypted format to another device.
@@ -40,30 +50,91 @@ def device_profile():
 #
 # Deliver sensor events as quickly as you want to simulate a sensor event
 def sensor_simulator(event_frequency):
-    # every milisecond an event comes in
-    if (event_frequency == 1):
+    events = 1000
+    while events >= 0:
+        
+        events -= 1
 
-    # every half second an event comes in
-    elif (event_frequency == 2):
-
-    # every second an event comes in
-    elif (event_frequency == 3):
-
-    # every 2 seconds an event comes in
-    elif (event_frequency == 4):
 #
 #
 #
+
+
+#
+#
+# generate a private key
+def gen_rsa_key():
+    # crypto library required variables
+    backend = default_backend()
+    # generate a private key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096,
+        backend=backend
+    )
+    # generate a public key
+    public_key = private_key.public_key()
+
+    # send over public key, message, and signed message
+    message = b"asdadw"
+    signed_message = private_key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+            hashes.SHA256()
+        )
+
+    # symmetric key setup
+    key = os.urandom(32)
+    iv = os.urandom(16)
+    algorithm = algorithms.AES(key)
+    mode_of_operation = modes.CBC(iv)
+
+    # generate symmetric cipher
+    cipher = Cipher(algorithm, mode_of_operation, backend=backend)
+    encryptor = cipher.encryptor()
+    
+    plain_text = b"a secret message"
+    cipher_text = encryptor.update(plain_text) + encryptor.finalize()
+    
+    # test decryption of the 
+    decryptor = cipher.decryptor()
+    decrypted_plain_text = decryptor.update(cipher_text) + decryptor.finalize()
+    print(decrypted_plain_text)
+    
+    # start sending encrypted data via process_telemtry() function
+
+
+    # verify signature
+    result = public_key.verify(
+        signed_message,
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    print(result)
+
+
+    #
+#
+#
+#
+
+gen_rsa_key()
 
 #
 #
 # process sensor telemetry (encrypt and send to database)
-def process_telemetry(simulator_output, device_details)
+def process_telemetry(simulator_output, device_details):
     # record start time
     start_time = int(round(time.time() * 1000))
     # bring in random data to simulate data ingested by a connected sensor
     # encrypt the data
-    
 
     # connect to database
 
